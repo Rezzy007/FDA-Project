6159d2aed81deb20168a1c1c1336870402f4dce1
Rezzy007 <emameziorezi@gmail.com>
2022-02-23 17:49:04 +0100

    first commit

---
+++ Javascript/signup.js
@@ -0,0 +1,20 @@
+var x = document.getElementById("signup");
+var y = document.getElementById("login");
+var z = document.getElementById("btn");
+
+const login = () =>{
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+
+var x = document.getElementById("signup");
+var y = document.getElementById("login");
+var z = document.getElementById("btn");
+
+const signup = () =>{
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
